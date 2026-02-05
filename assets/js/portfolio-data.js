082a8bb172ce88d3d1fb2df4fb10e4e0905680bc
Anselmo Dias <anselmodiasyahoo@gmail.com>
2026-02-04 21:20:44 -0300

    feat: configure portifolio details

--- assets/js/portfolio-data.js
+++ assets/js/portfolio-data.js
@@ -1,38 +1,75 @@
 const portfolioData = {
-    "social-media": {
+    "clinicas": {
         title: "Presença Digital",
         description: "Estratégias de mídia social para aumentar o engajamento e o alcance da marca.",
         images: [
-            "../assets/images/portfolio/presenca-digital-1.jpeg",
-            "../assets/images/portfolio/new-work-01.jpeg",
-            "../assets/images/portfolio/new-work-02.jpeg",
+            "../assets/images/portfolio/clinicas/4.jpeg",
+            "../assets/images/portfolio/clinicas/2.jpeg",
+            "../assets/images/portfolio/clinicas/3.jpeg",
+            "../assets/images/portfolio/clinicas/1.jpeg",
+            "../assets/images/portfolio/clinicas/5.jpeg",
         ]
     },
-    "post": {
+    "docerias": {
         title: "Criação de Posts",
         description: "Designs de posts criativos e informativos para redes sociais.",
         images: [
-            "../assets/images/portfolio/cards-01.jpeg",
-            "../assets/images/portfolio/new-work-03.jpeg",
-            "../assets/images/portfolio/new-work-04.jpeg",
+            "../assets/images/portfolio/docerias/1.jpeg",
+            "../assets/images/portfolio/docerias/2.jpeg",
+            "../assets/images/portfolio/docerias/3.jpeg",
         ]
     },
-    "link-personalizado": {
+    "identidade-de-perfil": {
         title: "Links Personalizados",
         description: "Criação de páginas de links personalizadas para agrupar todos os links importantes de uma marca.",
         images: [
-            "../assets/images/portfolio/link-01.jpeg",
-            "../assets/images/portfolio/portfolio-img-1.jpeg",
-            "../assets/images/portfolio/portfolio-img-2.jpeg",
+            "../assets/images/portfolio/identidade-de-perfil/1.jpeg",
+            "../assets/images/portfolio/identidade-de-perfil/2.jpeg",
+            "../assets/images/portfolio/identidade-de-perfil/3.jpeg",
+            "../assets/images/portfolio/identidade-de-perfil/4.jpeg",
         ]
     },
-    "marca-pessoal": {
+    "linkthree": {
         title: "Identidade de Perfil",
         description: "Desenvolvimento de uma identidade visual forte para perfis de marca pessoal.",
         images: [
-            "../assets/images/portfolio/identidade-01.jpeg",
-            "../assets/images/portfolio/portfolio-img-3.jpeg",
-            "../assets/images/portfolio/portfolio-img-4.jpeg",
+            "../assets/images/portfolio/linkthree/1.jpeg",
+            "../assets/images/portfolio/linkthree/3.jpeg",
+            "../assets/images/portfolio/linkthree/2.jpeg",
+        ]
+    },
+    "lojas": {
+        title: "Identidade de Perfil",
+        description: "Desenvolvimento de uma identidade visual forte para perfis de marca pessoal.",
+        images: [
+            "../assets/images/portfolio/lojas/1.jpeg",
+            "../assets/images/portfolio/lojas/2.jpeg",
+            "../assets/images/portfolio/lojas/3.jpeg",
+            "../assets/images/portfolio/lojas/3.jpeg",
+            "../assets/images/portfolio/lojas/4.jpeg",
+            "../assets/images/portfolio/lojas/5.jpeg",
+            "../assets/images/portfolio/lojas/6.jpeg",
+            "../assets/images/portfolio/lojas/7.jpeg",
+            "../assets/images/portfolio/lojas/10.jpeg",
+            "../assets/images/portfolio/lojas/8.jpeg",
+        ]
+    },
+    "mercados": {
+        title: "Identidade de Perfil",
+        description: "Desenvolvimento de uma identidade visual forte para perfis de marca pessoal.",
+        images: [
+            "../assets/images/portfolio/mercado/3.jpeg",
+            "../assets/images/portfolio/mercado/4.jpeg",
+            "../assets/images/portfolio/mercado/5.jpeg",
+            "../assets/images/portfolio/mercado/1.jpeg",
+            "../assets/images/portfolio/mercado/2.jpeg",
+            "../assets/images/portfolio/mercado/3.jpeg",
         ]
     }
 };
+
+
+
+
+
+
